fc855368f1d6346c896c3b3c72bf7f8ef8570ed4
Xinrui Wang <774603924@qq.com>
2022-12-16 14:06:00 +0800

    v1.5

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -18,13 +18,14 @@
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="test">角色</label>
-                        <input type="text" class="form-control" list="test" id="test2" value='学生'
+                        <input v-model="role" type="text" class="form-control" list="test" id="test2"
                             onfocus="this.value=''">
                         <datalist id="test">
                             <option value="学生">学生</option>
                             <option value="管理员">管理员</option>
                         </datalist>
                     </div>
+                    <div class="error_message">{{ error_message }}</div>
                     <button type="submit" class="btn btn-primary">注册</button>
                 </form>
 
@@ -37,6 +38,7 @@
 import ContentField from '@/components/ContentField.vue'
 import { ref } from 'vue'
 import $ from 'jquery'
+import router from '@/router'
 
 export default {
     components: {
@@ -46,7 +48,8 @@ export default {
         let sid = ref('')
         let username = ref('')
         let password = ref('')
-        let role = ref('')
+        let role = ref('学生')
+        let error_message = ref('用户已存在')
         const register = () => {
             $.ajax({
                 url: "http://127.0.0.1:3000/library/user/register",
@@ -60,6 +63,11 @@ export default {
                     role: role.value
                 }),
                 success(resp) {
+                    if(resp.status === "OK") {
+                        router.push({name : "login_view"})
+                    } else if(resp.status === "ERROR") {
+                        error_message.value = resp.reason
+                    }
                     console.log(resp)
                 },
                 error(resp) {
@@ -73,6 +81,7 @@ export default {
             username,
             password,
             role,
+            error_message,
             register,
         }
     }
@@ -83,5 +92,8 @@ export default {
 button {
     width: 100%;
     margin-top: 13px;
+}
+.error_message {
+    color: red;
 }
 </style>
